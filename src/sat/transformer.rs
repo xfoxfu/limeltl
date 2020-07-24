c4db23e04c53147a0d616e96595fea11ebd274be
Yuze Fu <i@xfox.me>
2020-07-24 18:35:04 +0800

    transform not

--- src/sat/transformer.rs
+++ src/sat/transformer.rs
@@ -1,4 +1,4 @@
-use crate::bool_logic::{BinaryOp, PropExpr, UnaryOp, Variable};
+use crate::bool_logic::{BinaryOp, PropExpr, UnaryOp};
 
 fn elim_impl_eq(val: PropExpr) -> PropExpr {
     match val {
@@ -26,7 +26,7 @@ fn elim_impl_eq(val: PropExpr) -> PropExpr {
         PropExpr::ChainedBinary(op, vals) => {
             PropExpr::ChainedBinary(op, vals.into_iter().map(|v| elim_impl_eq(v)).collect())
         }
-        _ => val,
+        PropExpr::Variable(_) => val,
     }
 }
 
@@ -37,26 +37,26 @@ fn elim_not(val: PropExpr) -> PropExpr {
             // double negation = value
             PropExpr::Unary(UnaryOp::Negation, rhs) => elim_not(*rhs),
             // apply De Morgan laws
-            PropExpr::Binary(lhs, BinaryOp::Conjunction, rhs) => !*lhs | !*rhs,
-            PropExpr::Binary(lhs, BinaryOp::Disjunction, rhs) => !*lhs & !*rhs,
+            PropExpr::Binary(lhs, BinaryOp::Conjunction, rhs) => elim_not(!*lhs) | elim_not(!*rhs),
+            PropExpr::Binary(lhs, BinaryOp::Disjunction, rhs) => elim_not(!*lhs) & elim_not(!*rhs),
             PropExpr::ChainedBinary(BinaryOp::Conjunction, vals) => PropExpr::ChainedBinary(
                 BinaryOp::Disjunction,
-                vals.into_iter().map(|v| !v).collect(),
+                vals.into_iter().map(|v| elim_not(!v)).collect(),
             ),
             PropExpr::ChainedBinary(BinaryOp::Disjunction, vals) => PropExpr::ChainedBinary(
                 BinaryOp::Conjunction,
-                vals.into_iter().map(|v| !v).collect(),
+                vals.into_iter().map(|v| elim_not(!v)).collect(),
             ),
             // other binary should be handled before
             PropExpr::Binary(_, _, _) => unreachable!(),
             PropExpr::ChainedBinary(_, _) => unreachable!(),
             // variable
-            var @ PropExpr::Variable(_) => var,
+            var @ PropExpr::Variable(_) => !var,
         },
         // recursion case
         #[allow(unreachable_patterns)] // guard this case for safety
         PropExpr::Unary(op, rhs) => match *rhs {
-            _ => PropExpr::Unary(op, Box::new(elim_impl_eq(*rhs))),
+            _ => PropExpr::Unary(op, Box::new(elim_not(*rhs))),
         },
         PropExpr::Binary(lhs, op, rhs) => {
             PropExpr::Binary(Box::new(elim_not(*lhs)), op, Box::new(elim_not(*rhs)))
@@ -64,7 +64,7 @@ fn elim_not(val: PropExpr) -> PropExpr {
         PropExpr::ChainedBinary(op, vals) => {
             PropExpr::ChainedBinary(op, vals.into_iter().map(|v| elim_not(v)).collect())
         }
-        _ => val,
+        PropExpr::Variable(_) => val,
     }
 }
 
@@ -72,39 +72,50 @@ fn elim_not(val: PropExpr) -> PropExpr {
 mod test {
     mod elim_impl_eq {
         use super::super::*;
+        use crate::bool_logic::Variable::And as V;
 
         #[test]
         fn plain() {
+            assert_eq!(elim_impl_eq(V(1) << (V(2) | V(3))), V(1) | !(V(2) | V(3)),);
+            assert_eq!(elim_impl_eq(V(1) >> (V(2) | V(3))), !V(1) | (V(2) | V(3)),);
             assert_eq!(
-                elim_impl_eq(Variable::And(1) << (Variable::And(2) | Variable::And(3)),),
-                Variable::And(1) | !(Variable::And(2) | Variable::And(3)),
+                elim_impl_eq(PropExpr::biconditional(V(1).into(), V(2) | V(3))),
+                (V(1) & (V(2) | V(3))) | (!V(1) & !(V(2) | V(3))),
             );
             assert_eq!(
-                elim_impl_eq(Variable::And(1) >> (Variable::And(2) | Variable::And(3)),),
-                !Variable::And(1) | (Variable::And(2) | Variable::And(3)),
-            );
-            assert_eq!(
-                elim_impl_eq(PropExpr::biconditional(
-                    Variable::And(1).into(),
-                    Variable::And(2) | Variable::And(3),
-                )),
-                (Variable::And(1) & (Variable::And(2) | Variable::And(3)))
-                    | (!Variable::And(1) & !(Variable::And(2) | Variable::And(3))),
+                elim_impl_eq(
+                    V(1) << PropExpr::chained_and(vec![V(2) << V(3), V(4) >> V(5), V(6).into()])
+                ),
+                V(1) | !PropExpr::chained_and(vec![V(2) | !V(3), !V(4) | V(5), V(6).into()])
             );
         }
 
         #[test]
         fn nested() {
             assert_eq!(
-                elim_impl_eq(
-                    Variable::And(1) << (Variable::And(2) | (Variable::And(3) << Variable::And(4))),
-                ),
-                Variable::And(1) | !(Variable::And(2) | (!Variable::And(3) | Variable::And(4))),
+                elim_impl_eq(V(1) << (V(2) | (V(3) >> V(4)))),
+                V(1) | !(V(2) | (!V(3) | V(4))),
             );
         }
     }
 
     mod elim_not {
         use super::super::*;
+        use crate::bool_logic::Variable::And as V;
+
+        #[test]
+        fn plain() {
+            assert_eq!(elim_not(!(V(1) | V(2))), !V(1) & !V(2));
+            assert_eq!(elim_not(!(V(1) & V(2))), !V(1) | !V(2));
+            assert_eq!(elim_not(!!V(1)), V(1).into());
+        }
+
+        #[test]
+        fn nested() {
+            assert_eq!(
+                elim_not(!(V(1) & (!V(2) & !(V(3) | V(4))))),
+                !V(1) | (V(2) | (V(3) | V(4)))
+            );
+        }
     }
 }
